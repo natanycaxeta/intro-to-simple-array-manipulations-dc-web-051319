36448c237ef8ba345ebe9549a1d6440b1837ae76
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 16:01:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,6 +17,7 @@ end
 
 def pop_with_args(array)
 chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
-
+chars_arya_killed = chars_in_game_of_thrones.pop(2, 3)
+end
   
   
