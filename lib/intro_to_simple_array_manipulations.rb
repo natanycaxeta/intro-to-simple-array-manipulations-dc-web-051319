a0d43d99ba4182c22b8aefe2c2b95cadc97fff10
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 17:15:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,6 +38,7 @@ end
 
 def using_insert(array, element)
   list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"
+  another_esoteric_language = "Malbolge"
   
 end 
   
