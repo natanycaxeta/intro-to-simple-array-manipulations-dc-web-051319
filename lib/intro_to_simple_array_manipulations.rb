aa76eb9fe17e1250b9ab8188ec25e080835531b8
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 16:53:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,5 +33,5 @@ end
 def using_concat(array, element)
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 more_favs = ["mario kart", "flatiron school"]
-
+all_my_favs = more_favs
   
