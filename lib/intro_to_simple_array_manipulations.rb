6bc2bb08f4c31fde99d9167329e42ebe33a3e45d
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 18:40:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,7 @@
 def using_push (countries_in_western_africa, next_countr)
   @countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   @next_country = "Niger"
-  @countries_in_western_africa.push(next_country)
+  @countries_in_western_africa.push(@next_country)
 end
 
 def using_unshift (array, string)
