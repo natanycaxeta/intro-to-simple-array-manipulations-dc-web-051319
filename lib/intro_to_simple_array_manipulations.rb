faec5ca59b079f6b7c8a462cb0f6eaa6c092c82d
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 18:51:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -51,4 +51,5 @@ end
    private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
    flat_array = private_colleges_in_newyork.flatten
  end
-   
+ 
+ def   
