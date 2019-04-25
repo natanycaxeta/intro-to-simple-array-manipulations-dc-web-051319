bcab3ef82aea4941a076ae9e637a10d49bab9a3c
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 15:55:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,13 +5,14 @@ countries_in_western_africa.push(next_country)
 end
 
 def using_unshift (array, string)
-  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   new_neighborhood = "Brooklyn Heights"
   updated_array = neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
  end
  
- def using_pop(array)
-   
+def using_pop(array)
+great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+
   
   
   
