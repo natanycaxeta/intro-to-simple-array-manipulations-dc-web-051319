6476ac99faf49c8be93227701d3816e4dea30cbe
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 18:39:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,18 +1,18 @@
 def using_push (countries_in_western_africa, next_countr)
-  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  next_country = "Niger"
-  countries_in_western_africa.push(next_country)
+  @countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  @next_country = "Niger"
+  @countries_in_western_africa.push(next_country)
 end
 
 def using_unshift (array, string)
-  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  new_neighborhood = "Brooklyn Heights"
-  updated_array = neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+  @neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  @new_neighborhood = "Brooklyn Heights"
+  @updated_array = @neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
  end
  
 def using_pop(great_hits_of_the_nineties)
-  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  deleted_string = great_hits_of_the_nineties.pop
+  @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  @deleted_string = @great_hits_of_the_nineties.pop
 end
 
 def pop_with_args(array)
