bd159e050e2f41d735fd27c6a716b927b6cf0764
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 18:41:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,4 +42,6 @@ def using_insert(list_of_esoteric_programming_languages, another_esoteric_langua
   @new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
 end
  
- def using_uniq ()
+ def using_uniq (captain_planet_and_the_planeteers)
+   captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+   
