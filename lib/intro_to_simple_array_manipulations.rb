65d645b55204bfa4315fda1038f89a0397b8672a
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 18:31:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,7 +36,7 @@ def using_concat(array, element)
   all_my_favs = my_favorite_things.concat(more_favs)
 end
 
-def using_insert(array, element)
+def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
   @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"
   @another_esoteric_language = "Malbolge"
   @new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
