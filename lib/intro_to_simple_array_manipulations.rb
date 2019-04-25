07241c970eb98bcb8da8eb675ec69b87214c2b70
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-25 18:40:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -41,4 +41,5 @@ def using_insert(list_of_esoteric_programming_languages, another_esoteric_langua
   @another_esoteric_language = "Malbolge"
   @new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
 end
-  
+ 
+ def  
